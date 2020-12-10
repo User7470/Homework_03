bf6ff6e3d0abcc3df8b2d33dd30c99af4243168d
Will Nelson <wn95880@gmail.com>
2020-12-10 05:10:22 -0500

    Cranked out a litte more than I should have all at once. Password generation is fully functional yet lacks some necessary final checks and could see optimization given I can figure out the means to do so

--- Develop/script.js
+++ Develop/script.js
@@ -13,14 +13,114 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Function for choosing which set of characters to use based on the confirms
+function decideCharset() {
+  //Horribly inefficient method, but all characters were put into their own arrays which will be combined based on the confirmations given by the user on their desired characters. Backslash didn't want to cooperate with this method and is not present. Space was also not included in this as I did not want it to end up at the start or end of a password (and also I don't believe many passwords actually want any spaces). The confirms also ended up moved into here whilst I was trying to find the source of a completely unrelated error but whatever.
+  var numConf = confirm("Would you like to include numbers in your password?");
+
+  var upperConf = confirm("Would you like to include uppercase letters in your password?");
+  
+  var lowerConf = confirm("Would you like to include lowercase letters in your password?");
+  
+  var symbConf = confirm("Would you like to include symbols in your password?");
+  if (numConf === false && upperConf === false && lowerConf === false && symbConf === false) {
+    alert("Please choose at least one character type!");
+    decideCharset();
+  }
+
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var symbols = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+  
+  var numUpper = numbers.concat(upperCase);
+  var numLower = numbers.concat(lowerCase);
+  var numSymb = numbers.concat(symbols);
+  var upperLower = upperCase.concat(lowerCase);
+  var upperSymb = upperCase.concat(symbols);
+  var lowerSymb = lowerCase.concat(symbols);
+  var numUpperLower = numbers.concat(upperCase, lowerCase);
+  var numUpperSymb = numbers.concat(upperCase, symbols);
+  var numLowerSymb = numbers.concat(lowerCase, symbols);
+  var upperLowerSymb = upperCase.concat(lowerCase, symbols);
+  var all = numbers.concat(upperCase, lowerCase, symbols);
+  
+  if (numConf === true && lowerConf === false && upperConf === false && symbConf === false) {
+    return numbers
+  }
+  else if (numConf === false && lowerConf === false && upperConf === true && symbConf === false) {
+    return upperCase
+  }
+  else if (numConf === false && lowerConf === true && upperConf === false && symbConf === false) {
+    return lowerCase
+  }
+  else if (numConf === false && lowerConf === false && upperConf === false && symbConf === true) {
+    return symbols
+  }
+  else if (numConf === true && lowerConf === true && upperConf === false && symbConf === false) {
+    return numLower
+  }
+  else if (numConf === true && lowerConf === false && upperConf === true && symbConf === false) {
+    return numUpper
+  }
+  else if (numConf === true && lowerConf === false && upperConf === false && symbConf === true) {
+    return numSymb
+  }
+  else if (numConf === false && lowerConf === true && upperConf === true && symbConf === false) {
+    return upperLower
+  }
+  else if (numConf === false && lowerConf === false && upperConf === true && symbConf === true) {
+    return upperSymb
+  }
+  else if (numConf === false && lowerConf === true && upperConf === false && symbConf === true) {
+    return lowerSymb
+  }
+  else if (numConf === true && lowerConf === true && upperConf === true && symbConf === false) {
+    return numUpperLower
+  }
+  else if (numConf === true && lowerConf === false && upperConf === true && symbConf === true) {
+    return numUpperSymb
+  }
+  else if (numConf === true && lowerConf === true && upperConf === false && symbConf === true) {
+    return numLowerSymb
+  }
+  else if (numConf === false && lowerConf === true && upperConf === true && symbConf === true) {
+    return upperLowerSymb
+  }
+  else {
+    return all
+  }
+  }
+
+
+
+
+
+
+
+
+
 function generatePassword() {
-// User must be prompted for password lenght (between 8 and 128) and prompted for the inclusion of upper and lower letters, numbers, and/or symbols. These values will be saved in variables and used later.
 
-// The lenght inputted will be probably be able to be used to form a loop that will loop through a process however many times needed to reach the needed number of characters
+// User is prompted for how many characters they want. Their answer is converted into a number and then passed through a check to determine if it is a valid answer. If the answer is not valid (Too small or large of a number or not a number at all), the function resets and the user is prompted again instead of being able to continue.
+var passLength = prompt("How many characters long do you want your password to be? (Between 8 and 128 characters only)")
 
-// For each loop a value will be pulled (I guess from an array or object of some sort?) using a random process, with the pool of characters being dependant on which prompts are marked as false or true
+var passLengthNum = parseInt(passLength, 10);
 
-// There should be a check afterwards to ensure that at least one symbol of each type specified as true has been chosen, this being if I have chosen to contain all possible characters at any given time within a few arrays meaning that there will be a chance present that one type doesn't get rolled even while specified. Either that or I create a better proccess to pick out and jumble the characters.
-
-// The characters chosen should be joined together into a single string at the end of the process and returned at the end of the function. The code is already present to show the result of this function as text and hopefully nothing further will have to be done beyond this point.
+if (passLengthNum < 8 || passLengthNum > 128 || isNaN(passLengthNum) === true ) {
+generatePassword();
 }
+//Password Generation Finale
+else {
+  var charSet = decideCharset();
+  var frigginPassword = " "
+  for (i = 0; i < passLengthNum; i++) {
+    
+    frigginPassword += charSet[Math.floor(Math.random() * charSet.length)];
+}
+  return frigginPassword
+}
+}
+
+// Passwords need to be checked for presence of all characters specified, with current methods it is possible to miss a given character at random. I will look into this, however at the very least passwords can still be generated just with a chance of imperfection.
+
